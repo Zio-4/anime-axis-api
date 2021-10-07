247c3b1d3c9a3103a5b6d251d8180d58395cc35d
Philip Ziolkowski <philipz4848@gmail.com>
2021-10-06 20:48:59 -0700

    Adding login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   resources :mangas
   resources :animes
   resources :users
+
+  post "/login", to: "sessions#create"
+  get "/user", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20211004182123_create_forum_posts.rb
+++ db/migrate/20211004182123_create_forum_posts.rb
@@ -3,8 +3,8 @@ class CreateForumPosts < ActiveRecord::Migration[6.1]
     create_table :forum_posts do |t|
       t.belongs_to :forum, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
-      t.belongs_to :anime, null: false, foreign_key: true
-      t.belongs_to :manga, null: false, foreign_key: true
+      t.belongs_to :anime, foreign_key: true
+      t.belongs_to :manga, foreign_key: true
       t.string :title
       t.text :content
 
