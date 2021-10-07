2cb95aac6280f2a8c0b694e719daa4b2a23db788
Philip Ziolkowski <philipz4848@gmail.com>
2021-10-06 21:02:29 -0700

    Creating controller actions for authentication and authorization

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+    skip_before_action :authorize, only: [:create]
+    
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {error: "Invalid Username or Password. Try again"}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
