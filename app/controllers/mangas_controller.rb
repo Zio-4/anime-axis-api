ab6dac9db776abda5cbbdd4b22cab08f9e0ebdbe
Philip Ziolkowski <philipz4848@gmail.com>
2021-10-25 16:46:13 -0700

    Adding create controller action for mangas to be later used for manga list

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -1,2 +1,20 @@
 class MangasController < ApplicationController
+
+    def create
+        if Manga.find_by(id: params[:id])
+            # If an anime already exists in the Anime table, only add it to the users anime list unless it is already in the users list
+            @current_user.mangas << Manga.find_by(id: params[:id]) unless @current_user.mangas.each { |m| m.id == params[:id]}
+        else
+            # If an anime is not in the Anime table, create it and add it to the users anime list 
+            manga = Manga.create!(manga_params)
+            @current_user.mangas << manga
+            render json: manga, status: :created
+        end
+    end
+
+    private
+
+    def manga_params
+        params.permit(:title, :id, :image_url, :score)
+    end
 end
