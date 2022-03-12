9d2a8e6f47c0c861ec0aa8ccaadb53dd59c471b7
Philip Ziolkowski <philipz4848@gmail.com>
2022-03-11 23:52:33 -0800

    setting up for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   get "/user", to: "users#show"
   delete "/logout", to: "sessions#destroy"
   post "/signup", to: "users#create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
 
