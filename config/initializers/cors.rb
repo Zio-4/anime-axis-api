1dc826b8b7094ce09ab31c4666c6e9d86e852235
Philip Ziolkowski <philipz4848@gmail.com>
2022-03-12 23:14:12 -0800

    changing origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://anime-axis.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
