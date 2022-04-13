2b3d6bc67e12e73651bf26a04e504372cc621217
Philip Ziolkowski <philipz4848@gmail.com>
2022-04-13 16:19:23 -0700

    Adding debug test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
     def update
         user = User.find_by(id: params[:id])
+        logger.debug "****test****"
         if user.update(user_params)
             render json: user, status: :ok
         else
