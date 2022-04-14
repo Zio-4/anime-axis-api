d035e42b5b59a3fe11f794f3b267c19cc1ec6c44
Philip Ziolkowski <philipz4848@gmail.com>
2022-04-14 10:47:17 -0700

    Adding serialize hash

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :bio, :avatar, :animes, :mangas
 
+  def serializable_hash(adapter_options = nil, options = {}, adapter_instance = self.class.serialization_adapter_instance)
+    hash = super
+    hash.each { |key, value| hash.delete(key) if value.nil? }
+    hash
+  end
 end
