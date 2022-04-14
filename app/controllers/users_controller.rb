ed95f1b1766e3a6b3c5bc6fd23005e78708a5160
Philip Ziolkowski <philipz4848@gmail.com>
2022-04-14 12:37:44 -0700

    Adding debug test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
         if @current_user.update!(user_params)
             render json: user, status: :ok
         else
-            p @current_user.errors
+            logger.debug "*****test******"
             render json: @current_user.errors, status: :unprocessable_entity
         end
     end
